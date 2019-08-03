011830d8acfecbf69aaf07be5ea9be432bcc399b
Chris <dermongolola@gmail.com>
2019-08-03 17:48:06 +0200

    Added Basic Routing and Shop Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {ButtonModule} from 'primeng/button';
+import { RouterModule } from "@angular/router";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule
+    ButtonModule,
+    RouterModule.forRoot([
+      {path: 'shop', component: ShopComponent}
+    ])
   ],
   
   providers: [],
